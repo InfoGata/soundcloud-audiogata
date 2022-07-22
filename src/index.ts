a5eed12d0551460bd38ba4fb60cb7343cbf5f390
Elijah Green <elijahgreen@gmail.com>
2022-07-22 16:08:32 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import Soundcloud, { SoundcloudTrackV2 } from "soundcloud.ts";
+import axios from "axios";
+import "audiogata-plugin-typings";
+let soundcloud: Soundcloud | undefined;
+
+const getArtwork = (track: SoundcloudTrackV2): ImageInfo[] => {
+  let artwork = track.artwork_url ? track.artwork_url : track.user.avatar_url;
+  return [
+    { url: artwork, height: 100, width: 100 },
+    { url: artwork.replace("-large", "-t500x500"), height: 500, width: 500 },
+  ];
+};
+async function searchTracks(
+  request: SearchRequest
+): Promise<SearchTrackResult> {
+  const limit = 50;
+  const trackSearch = await soundcloud?.tracks.searchV2({
+    q: request.query,
+    limit,
+  });
+
+  const tracks: Track[] | undefined = trackSearch?.collection.map((c) => ({
+    name: c.title,
+    apiId: c.id.toString(),
+    duration: c.duration / 1000,
+    artistName: c.label_name || "",
+    source: c.media.transcodings.find(
+      (t) =>
+        t.format.mime_type === "audio/mpeg" &&
+        t.format.protocol === "progressive"
+    )?.url,
+    images: getArtwork(c),
+  }));
+  return {
+    items: tracks?.filter((t) => !!t.source) || [],
+  };
+}
+
+async function searchAll(request: SearchRequest): Promise<SearchAllResult> {
+  const [tracks] = await Promise.all([searchTracks(request)]);
+  return { tracks };
+}
+
+async function getTrackByUrl(track: Track): Promise<string> {
+  const corsProxy = await application.getCorsProxy();
+  let clientId = await soundcloud?.api.getClientID();
+  const source = track.source || "";
+  let url = "";
+  let connect = source?.includes("secret_token")
+    ? `&client_id=${clientId}`
+    : `?client_id=${clientId}`;
+  try {
+    url = await axios.get(corsProxy + source + connect).then((r) => r.data.url);
+  } catch {
+    clientId = await soundcloud?.api.getClientID(true);
+    connect = source?.includes("secret_token")
+      ? `&client_id=${clientId}`
+      : `?client_id=${clientId}`;
+    url = await axios.get(corsProxy + source + connect).then((r) => r.data.url);
+  }
+
+  return url;
+}
+
+const init = async () => {
+  let corsProxy = await application.getCorsProxy();
+  if (!corsProxy) {
+    corsProxy = "https://cloudcors.audio-pwa.workers.dev/";
+  }
+  soundcloud = new Soundcloud(undefined, undefined, {
+    proxy: corsProxy,
+  });
+
+  application.onSearchAll = searchAll;
+  application.onSearchTracks = searchTracks;
+  application.onGetTrackUrl = getTrackByUrl;
+};
+
+init();
